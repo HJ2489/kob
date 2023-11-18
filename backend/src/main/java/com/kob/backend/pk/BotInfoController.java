d4d7221e5e7d234c55966b7e797f5a6c30d5494b
HJ2489 <2489436188@qq.com>
2023-11-18 22:00:02 +0800

    项目骨架搭建

--- backend/src/main/java/com/kob/backend/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/pk/BotInfoController.java
@@ -3,6 +3,9 @@ package com.kob.backend.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author: Torch
  * @Date: 2023/11/17 22:34
@@ -13,7 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class BotInfoController {
 
     @RequestMapping("getbotinfo/")
-    public String getBotInfo(){
-        return "hhhhhhh";
+    public Map<String, String> getBotInfo(){
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name", "tiger");
+        bot1.put("rating", "1500");
+        return bot1;
     }
 }

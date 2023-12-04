ef29c4fb7153bc6d296021eed5074f89b8485e3b
HJ2489 <2489436188@qq.com>
2023-12-04 18:20:21 +0800

    画蛇和处理键盘输入操作

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -1,5 +1,6 @@
 import { AcGameObject } from "./AcGameObject";  // export default的import就不用{}，不过每个只能export default一次
 import { Wall } from "./Wall"
+import { Snake } from "./Snake"
 
 
 export class GameMap extends AcGameObject {
@@ -10,21 +11,28 @@ export class GameMap extends AcGameObject {
         this.parent = parent;
         this.L = 0;
 
-        this.cols = 13;
+        this.cols = 14;
         this.rows = 13;
 
         this.inner_walls_count = 20;
         this.walls = [];
+
+        this.snakes = [
+            new Snake({ id: 0, color: "#4876EC", r: this.rows - 2, c: 1 }, this),
+            new Snake({ id: 1, color: "#F94848", r: 1, c: this.cols - 2 }, this),
+        ];
+
     }
 
+
     check_connectivity(g, sx, sy, tx, ty) {
-        if(sx == tx && sy == ty) return true;
+        if (sx == tx && sy == ty) return true;
         g[sx][sy] = true;
 
         let dx = [-1, 0, 1, 0], dy = [0, 1, 0, -1];
-        for(let i = 0; i < 4; i++) {
+        for (let i = 0; i < 4; i++) {
             let x = sx + dx[i], y = sy + dy[i];
-            if(!g[x][y] && this.check_connectivity(g, x, y, tx, ty))
+            if (!g[x][y] && this.check_connectivity(g, x, y, tx, ty))
                 return true;
         }
 
@@ -45,7 +53,7 @@ export class GameMap extends AcGameObject {
             g[r][0] = g[r][this.cols - 1] = true;
         }
         for (let c = 0; c < this.cols; c++) {
-            g[0][c] = g[this.cols - 1][c] = true;
+            g[0][c] = g[this.rows - 1][c] = true;
         }
 
         // 创建随机障碍物
@@ -53,11 +61,11 @@ export class GameMap extends AcGameObject {
             for (let j = 0; j < 1000; j++) {
                 let r = parseInt(Math.random() * this.rows);
                 let c = parseInt(Math.random() * this.cols);
-                if (g[r][c] || g[c][r]) continue;
+                if (g[r][c] || g[this.rows - 1 - r][this.cols - 1 - c]) continue;
                 if (r == this.rows - 2 && c == 1 || r == 1 && c == this.cols - 2)
                     continue;
 
-                g[r][c] = g[c][r] = true;
+                g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = true;
                 break;
             }
         }
@@ -76,20 +84,78 @@ export class GameMap extends AcGameObject {
         return true;
     }
 
+    add_listening_events() {
+        this.ctx.canvas.focus();
+
+        const [snake0, snake1] = this.snakes;
+        this.ctx.canvas.addEventListener("keydown", e => {
+            if (e.key === 'w') snake0.set_direction(0);
+            else if (e.key === 'd') snake0.set_direction(1);
+            else if (e.key === 's') snake0.set_direction(2);
+            else if (e.key === 'a') snake0.set_direction(3);
+            else if (e.key === 'ArrowUp') snake1.set_direction(0);
+            else if (e.key === 'ArrowRight') snake1.set_direction(1);
+            else if (e.key === 'ArrowDown') snake1.set_direction(2);
+            else if (e.key === 'ArrowLeft') snake1.set_direction(3);
+        });
+    }
+
     start() {
         for (let i = 0; i < 1000; i++)
             if (this.create_walls())
                 break;
+
+        this.add_listening_events();
     }
 
     update_size() {
+        // L是由外层的div：parent计算得到的每个格子的边长
         this.L = parseInt(Math.min(this.parent.clientWidth / this.cols, this.parent.clientHeight / this.rows));
         this.ctx.canvas.width = this.L * this.cols;
         this.ctx.canvas.height = this.L * this.rows;
     }
 
+    check_ready() {  // 判断两条蛇是否都准备好下一回合了
+        for (const snake of this.snakes) {
+            if (snake.status !== "idle") return false;
+            if (snake.direction === -1) return false;
+        }
+        return true;
+    }
+
+    next_step() {  // 让两条蛇进入下一回合
+        for (const snake of this.snakes) {
+            snake.next_step();
+        }
+    }
+
+    check_valid(cell) {  // 检测目标位置是否合法；没有撞到两条蛇的身体和障碍物
+        for (const wall of this.walls) {
+            if (wall.r === cell.r && wall.c === cell.c)
+                return false;
+        }
+
+        for (const snake of this.snakes) {
+            let k = snake.cells.length;
+            if (!snake.check_tail_increasing()) {  // 当蛇尾会前进(即蛇不会变长)的时候，蛇尾不要判断
+                k--;  // k表示的是snake.cells[k]表示的是蛇尾最初的位置，不会变长，蛇尾移动到k-1,所以k--，下面的判断
+            }
+            for (let i = 0; i < k; i++) {
+                // 往回走的也包含在内了,
+                // 由于是对两条蛇进行遍历，而check函数是每条蛇都会执行的，故也能检测出撞到别的蛇，即目标cell与另一条蛇身体重合
+                if (snake.cells[i].r === cell.r && snake.cells[i].c === cell.c)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
     update() {
         this.update_size();
+        if (this.check_ready()) {
+            this.next_step();
+        }
         this.render();
     }
 
